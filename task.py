702687c12d95e794156e03368b1a61c787930a42
Vianney Lotoy <vianneylotoy@users.noreply.github.com>
2017-08-25 23:14:08 +0200

    Tasks python class

---
+++ task.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from random import uniform
+
+
+class Tasks:
+	#Attribute of instances of the class
+	def __init__(self):
+		self.pi = 0
+		self.φi = 0
+		self.si = -1
+		self.Ci = 0
+		
+		
+	
+			
+			
+	
+		
